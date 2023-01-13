af91683076eba9165ada932c3124f3bf492b3297
Tommertom <Tom.Gruintjes@gmail.com>
2023-01-13 19:39:28 +0100

    hellos2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 
 	<footer>
 		<p>
-			visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to HELLOS SvelteKit
+			visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to HssELLOS SvelteKit
 		</p>
 	</footer>
 </div>
